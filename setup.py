dd61847a3a574dc2f58896c0679f3de016d796d3
Ritika Panchal <145095702+RitikaPanchal01@users.noreply.github.com>
2025-06-10 11:46:41 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup (
+name = 'Generative AI Project',
+version= '0.0.0', 
+author= 'Ritika Panchal', 
+author_email= 'panchalritika80@gmail.com' ,
+packages= find_packages(),
+install_requires = []
+
+)
